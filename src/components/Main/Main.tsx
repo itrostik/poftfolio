e22d382b4eb2a217accf35cdbd8800202882276a
itrostik <RosASergeev@stud.kpfu.ru>
2023-11-24 19:30:21 +0300

    что-то не хочется ничего делать

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,12 +1,14 @@
 import styles from "./main.module.scss";
 import Info from "../../screens/Info/Info.tsx";
 import Projects from "../../screens/Projects/Projects.tsx";
+import Stack from "../Stack/Stack.tsx";
 
 export default function Main() {
   return (
     <div className={styles["main"]}>
       <Info />
       <Projects />
+      <Stack />
     </div>
   );
 }
