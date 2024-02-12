d3eee2186848a0f8a40e4dc88720e308756c3223
itrostik <RosASergeev@stud.kpfu.ru>
2024-02-12 19:01:02 +0300

    картинка слишком много весила

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,10 +1,10 @@
-import avatar from "/avatar.svg";
+import avatar from "/avatar.jpg";
 import styles from "./Profile.module.scss";
 export default function Profile() {
   return (
     <div className={styles.wrapper}>
       <h1>itrostik</h1>
-      <img src={avatar} alt="avatar" />
+      <img src={avatar} alt="avatar" className={styles.logo} />
       <span>Привет, меня зовут Ростислав</span>
       <span>Я frontend-разработчик</span>
     </div>
