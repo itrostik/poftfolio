6c90df98e36a9bdca192fb462b698ff6798e10aa
itrostik <rostislavsergeev.it@gmail.com>
2024-04-21 21:11:24 +0300

    +leetcode

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,6 +4,7 @@ import styles from "./Profile.module.scss";
 import telegram from "../../assets/telegram.svg";
 import vk from "../../assets/vk.svg";
 import github from "../../assets/github.svg";
+import leetcode from "../../assets/leetcode.svg";
 export default function Profile() {
   return (
     <div className={styles.wrapper}>
@@ -12,14 +13,17 @@ export default function Profile() {
       <span>Привет, меня зовут Ростислав</span>
       <span>Я frontend-разработчик</span>
       <div className={styles.contacts}>
+        <a href={"https://leetcode.com/itrostik/"} className={styles.contact}>
+          <img src={leetcode} alt=""/>
+        </a>
         <a href={"https://github.com/itrostik"} className={styles.contact}>
-          <img src={github} alt="" />
+          <img src={github} alt=""/>
         </a>
         <a href={"https://t.me/itrostik"} className={styles.contact}>
-          <img src={telegram} alt="" />
+          <img src={telegram} alt=""/>
         </a>
         <a href={"https://vk.com/itrostik"} className={styles.contact}>
-          <img src={vk} alt="" />
+          <img src={vk} alt=""/>
         </a>
       </div>
     </div>
